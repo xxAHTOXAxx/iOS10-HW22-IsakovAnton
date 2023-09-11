090265b54ff9fdb1ae48d3d89390c81f743d540d
xxAHTOXAxx <133795951+xxAHTOXAxx@users.noreply.github.com>
2023-09-11 23:54:33 +0400

    settingProject

--- iOS10-HW22 IsakovAnton/ViewController.swift
+++ iOS10-HW22 IsakovAnton/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  iOS10-HW22 IsakovAnton
-//
-//  Created by Антон Исаков on 11.09.2023.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
     }
 
 
